1a3edbcb8b69f57c3a98f612891c9322e4d3e990
lovebaicai <cfqtxd1@hotmail.com>
2017-07-18 21:38:41 +0800

    update

--- user_agents.py
+++ user_agents.py
@@ -1,6 +1,6 @@
 # encoding=utf-8
 
-""" User-Agents """"
+""" User-Agents """
 
 agents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
